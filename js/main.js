97d2a3432b64edbfb4de14a6f23f4529ac50e21c
Water-may <nugasdangal@gmail.com>
2022-12-05 19:21:32 +0545

    under construction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var i = 0;
+var txt = 'Under Construction 👷.';
+var speed = 100;
+
+function typeWriter() {
+    if (i < txt.length) {
+    document.getElementById("note").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector("#nav");
     const ham = document.querySelector("#ham");
@@ -12,4 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
         ham.style.display = "block";
         nav.style.display = "none";
     });
+    typeWriter();
 });
+
+
+

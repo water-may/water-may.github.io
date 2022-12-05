93d59c983fae0b771d405c0624215d1712bde8c5
Water-may <nugasdangal@gmail.com>
2022-12-05 18:54:40 +0545

    link bar done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#ham").style.display = "none";
+    const nav = document.querySelector("#nav");
+    const ham = document.querySelector("#ham");
+    const cross = document.querySelector("#cross");
+    ham.style.display = "block";
+    nav.style.display = "none";
+    ham.addEventListener("click", () => {
+        ham.style.display = "none";
+        nav.style.display = "flex";
+    });
+    cross.addEventListener("click", () => {
+        ham.style.display = "block";
+        nav.style.display = "none";
+    });
 });
